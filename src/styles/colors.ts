d54b62487c186642910af777fa79b87bb4385035
Mahmud Bagheri <dr.cwna@gmail.com>
2020-10-30 21:31:42 +0330

    add theme colors

---
+++ src/styles/colors.ts
@@ -0,0 +1,22 @@
+const __COLORS__ = {
+    grey: {
+        100: "#F9F9F9",
+        200: "#D5D8DA",
+        300: "#B2B8BA",
+        400: "#8E979B",
+        500: "#6B777C",
+        600: "#47565D",
+        700: "#24363D",
+        800: "#00151E",
+    },
+    red: "#EB5C2F",
+    green: "#1CBF79",
+    orange: "#F1B651",
+    lightBlue: "#D9ECF2",
+    midBlue: "#2271B3",
+    darkBlue: "#063972",
+    black: "#000",
+    white: "#fff",
+};
+
+export default __COLORS__;
