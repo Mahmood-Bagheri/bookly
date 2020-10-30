6fb59174bf9237ffb413188ced6c76e9c227fea7
Mahmud Bagheri <dr.cwna@gmail.com>
2020-10-30 21:33:04 +0330

    change order of imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
-import reportWebVitals from "./reportWebVitals";
 import React from "react";
+import reportWebVitals from "./reportWebVitals";
 import ReactDOM from "react-dom";
 
 // * main app component
