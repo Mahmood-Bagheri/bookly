da5f814c8bd34d350672941a34b7432cee4bddab
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 11:40:19 +0330

    add bookbox to intro page

--- src/views/app/dashboard/Intro.tsx
+++ src/views/app/dashboard/Intro.tsx
@@ -1,6 +1,14 @@
 import React from "react";
-import { Book } from "components/Book";
+import { BookBox } from "components/BookBox";
 
 export default function App() {
-    return <Book />;
+    return (
+        <div className="container mt-2">
+            <div className="flex space-s-0 md:space-s-4 flex-wrap md:flex-no-wrap">
+                <div className="w-full md:w-1/2 lg:w-1/4 mb-2">
+                    <BookBox />
+                </div>
+            </div>
+        </div>
+    );
 }
