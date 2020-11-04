95cc49aea631bea8dee11b54795348cd026b1a76
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 11:30:39 +0330

    initiate the book component

--- src/components/BookBox/BookBox.tsx
+++ src/components/BookBox/BookBox.tsx
@@ -1,7 +1,23 @@
 import React, { FunctionComponent } from "react";
 import { BookBoxComponentProps } from "./BookBox.types";
+import BookImage from "assets/images/book.jpg";
 import s from "./BookBox.module.scss";
 
 export const BookBox: FunctionComponent<BookBoxComponentProps> = props => {
-    return <div></div>;
+    return (
+        <div className={s.box}>
+            <div className={s.publisher}>فروشگاه مبل توکا</div>
+            <img className={s.image} src={BookImage} />
+            <div className={s.content}>
+                <div>
+                    <div className={s.title}>مبل آنتیک</div>
+                    <div className={s.code}>aq-128</div>
+                </div>
+                <div className={s.priceWrap}>
+                    <div className={s.price}>12,000</div>
+                    <div className={s.priceUnit}>تومان</div>
+                </div>
+            </div>
+        </div>
+    );
 };
