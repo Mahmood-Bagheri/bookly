26d728b9c74f64d6a3cd39a2733ceb8025d77d04
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 21:38:28 +0330

    add components of book carousel

--- src/components/BooksCarousel/BooksCarousel.tsx
+++ src/components/BooksCarousel/BooksCarousel.tsx
@@ -1,7 +1,49 @@
 import React, { FunctionComponent } from "react";
+import SwiperCore, { Autoplay } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import Image from "assets/images/book.jpg";
 import { BooksCarouselComponentProps } from "./BooksCarousel.types";
 import s from "./BooksCarousel.module.scss";
 
+import "swiper/swiper.scss";
+import "swiper/components/pagination/pagination.scss";
+
+SwiperCore.use([Autoplay]);
+
 export const BooksCarousel: FunctionComponent<BooksCarouselComponentProps> = props => {
-    return <div></div>;
+    const swiperProps = {
+        spaceBetween: 30,
+        slidesPerView: 1.45,
+        loop: true,
+        autoplay: {
+            delay: 2000,
+        },
+        className: "mt-2",
+    };
+
+    return (
+        <Swiper {...swiperProps}>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+                <img src={Image} className={s.imageSlide} alt="" />
+            </SwiperSlide>
+        </Swiper>
+    );
 };
