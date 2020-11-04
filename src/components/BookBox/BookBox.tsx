f72cbb8ec3002a24e858826b2bdb0146727b31fa
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 13:30:36 +0330

    update BookBox

--- src/components/BookBox/BookBox.tsx
+++ src/components/BookBox/BookBox.tsx
@@ -1,22 +1,27 @@
 import React, { FunctionComponent } from "react";
 import { BookBoxComponentProps } from "./BookBox.types";
 import BookImage from "assets/images/book.jpg";
+import { LikeButton } from "components/LikeButton";
+import { BookmarkButton } from "components/BookmarkButton";
 import s from "./BookBox.module.scss";
 
 export const BookBox: FunctionComponent<BookBoxComponentProps> = props => {
     return (
         <div className={s.box}>
-            <div className={s.publisher}>فروشگاه مبل توکا</div>
             <img className={s.image} src={BookImage} />
             <div className={s.content}>
                 <div>
-                    <div className={s.title}>مبل آنتیک</div>
+                    <div className={s.title}>جز از کل</div>
                     <div className={s.code}>aq-128</div>
                 </div>
-                <div className={s.priceWrap}>
-                    <div className={s.price}>12,000</div>
-                    <div className={s.priceUnit}>تومان</div>
+                <div>
+                    <div className={s.author}>استیو تولتز</div>
+                    <div className={s.authorTitle}>نویسنده</div>
                 </div>
+            </div>
+            <div className={s.actions}>
+                <LikeButton />
+                <BookmarkButton />
             </div>
         </div>
     );
