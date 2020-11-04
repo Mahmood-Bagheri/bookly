4ee254857550ec0af37b08efddc7c49db8d1948e
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 11:42:14 +0330

    add global styles

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,7 @@ import App from "App";
 
 // * main style
 import "assets/fonts/dana/fontiran.css";
+import "assets/styles/global.scss";
 
 /* recoil service */
 import { RecoilService } from "services/recoil/recoilService";
