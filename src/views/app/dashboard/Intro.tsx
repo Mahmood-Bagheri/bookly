1d1b61f6240dbcd1bedf022abd2b258247a7c55b
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-04 18:02:14 +0330

    add comment box to intro

--- src/views/app/dashboard/Intro.tsx
+++ src/views/app/dashboard/Intro.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { BookBox } from "components/BookBox";
+import { CommentBox } from "components/CommentBox";
 
 export default function App() {
     return (
-        <div className="container mt-2">
-            <div className="flex space-s-0 md:space-s-4 flex-wrap md:flex-no-wrap">
-                <div className="w-full md:w-1/2 lg:w-1/4 mb-2">
-                    <BookBox />
+        <div className="container mt-5">
+            <div className="row">
+                <div className="col-12">
+                    <CommentBox />
                 </div>
             </div>
         </div>
