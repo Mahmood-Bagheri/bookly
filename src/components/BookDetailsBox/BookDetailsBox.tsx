568afe49bd89539ccf4fc735bec51952bf449be5
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 13:00:03 +0330

    remove book BookDetailsCountBox

--- src/components/BookDetailsBox/BookDetailsBox.tsx
+++ src/components/BookDetailsBox/BookDetailsBox.tsx
@@ -1,6 +1,5 @@
 import React, { FunctionComponent } from "react";
 import { BooksCarousel } from "components/BooksCarousel";
-import { BookDetailsCountBox } from "components/BookDetailsCountBox";
 import { BookDetailsBoxComponentProps } from "./BookDetailsBox.types";
 import s from "./BookDetailsBox.module.scss";
 
@@ -12,19 +11,6 @@ export const BookDetailsBox: FunctionComponent<BookDetailsBoxComponentProps> = p
                 <div className={s.code}>GCP-11299</div>
             </div>
             <BooksCarousel />
-
-            <div className="p-3">
-                <BookDetailsCountBox
-                    type="like"
-                    count={150}
-                    description="نفر این پست را پسندیده اند"
-                />
-                <BookDetailsCountBox
-                    type="comment"
-                    count={45}
-                    description="نفر در مورد این پست نظر داده اند"
-                />
-            </div>
         </div>
     );
 };
