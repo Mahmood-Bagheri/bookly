b0c79fe82332a7c0e9def9ddf050f0913ac4c4f7
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 13:45:38 +0330

    add a randomDate generator utility

--- src/helpers/randomDate.ts
+++ src/helpers/randomDate.ts
@@ -1,3 +1,8 @@
+/**
+ * @param description This utility generates a random date between the given start and end date
+ * @param start the start date
+ * @param end the end date
+ */
 export const randomDate = (
     start: Date = new Date(),
     end: Date = new Date()
