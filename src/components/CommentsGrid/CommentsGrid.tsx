32dd6358320e66122e93be766009eb68eb1a7ad6
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 13:00:34 +0330

    update comments grid

--- src/components/CommentsGrid/CommentsGrid.tsx
+++ src/components/CommentsGrid/CommentsGrid.tsx
@@ -1,7 +1,16 @@
 import React, { FunctionComponent } from "react";
 import { CommentsGridComponentProps } from "./CommentsGrid.types";
 import s from "./CommentsGrid.module.scss";
+import { CommentBox } from "components/CommentBox";
 
 export const CommentsGrid: FunctionComponent<CommentsGridComponentProps> = props => {
-    return <div></div>;
+    return (
+        <div className={s.box}>
+            <CommentBox />
+            <CommentBox />
+            <CommentBox />
+            <CommentBox />
+            <CommentBox />
+        </div>
+    );
 };
