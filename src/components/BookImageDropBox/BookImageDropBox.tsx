b7cbcccaea2e8df2c084ebb4d1b608340412d4ec
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 17:37:46 +0330

    add book image dropbox styles

--- src/components/BookImageDropBox/BookImageDropBox.tsx
+++ src/components/BookImageDropBox/BookImageDropBox.tsx
@@ -1,7 +1,18 @@
 import React, { FunctionComponent } from "react";
+import { ReactComponent as ImaegIcon } from "assets/icons/image.svg";
 import { BookImageDropBoxComponentProps } from "./BookImageDropBox.types";
 import s from "./BookImageDropBox.module.scss";
 
 export const BookImageDropBox: FunctionComponent<BookImageDropBoxComponentProps> = props => {
-    return <div></div>;
+    return (
+        <div className={`${s.box} shadow`}>
+            <div className={s.content}>
+                <ImaegIcon className={s.icon} />
+                <div className={s.title}>تصویر را اینجا وِل کنید</div>
+                <div className={s.description}>
+                    حداکثر حجم عکس باید 500 کیلوبایت باشد
+                </div>
+            </div>
+        </div>
+    );
 };
