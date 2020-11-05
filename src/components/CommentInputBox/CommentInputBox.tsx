8550f05c48a810d51c52eda126f03ea43b42b068
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 14:41:52 +0330

    replace pure input component with the custom one

--- src/components/CommentInputBox/CommentInputBox.tsx
+++ src/components/CommentInputBox/CommentInputBox.tsx
@@ -1,15 +1,15 @@
 import React, { FunctionComponent } from "react";
+import { Input } from "components/Input";
 import { CommentInputBoxComponentProps } from "./CommentInputBox.types";
-import s from "./CommentInputBox.module.scss";
 import { Button } from "components/Button";
+import s from "./CommentInputBox.module.scss";
 
 export const CommentInputBox: FunctionComponent<CommentInputBoxComponentProps> = props => {
     return (
         <div className={s.box}>
             <p className={s.title}>نظرتون در مورد این پست چیه؟</p>
-            <textarea
+            <Input
                 rows={7}
-                className={s.input}
                 placeholder="مثلا: من این کتاب رو خیلی دوست دارم :)"
             />
             <Button type="primary">ارسال نظر</Button>
