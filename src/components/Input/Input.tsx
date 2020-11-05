5d9f1b9053449a0c482bd632ad99cd02fcc6f825
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 14:41:14 +0330

    update custom input component

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,7 +1,24 @@
 import React, { FunctionComponent } from "react";
 import { InputComponentProps } from "./Input.types";
+import classnames from "classnames";
 import s from "./Input.module.scss";
 
 export const Input: FunctionComponent<InputComponentProps> = props => {
-    return <div></div>;
+    const { className = "", ...restProps } = props;
+    const clsx = classnames(s.input, className);
+
+    if ("rows" in props) {
+        return <textarea {...props} className={clsx} />;
+    }
+
+    return (
+        <input
+            type="text"
+            className={clsx}
+            {...(props as React.DetailedHTMLProps<
+                React.InputHTMLAttributes<HTMLInputElement>,
+                HTMLInputElement
+            >)}
+        />
+    );
 };
--- src/components/Input/Input.types.d.ts
+++ src/components/Input/Input.types.d.ts
@@ -1,1 +1,9 @@
-export type InputComponentProps = {};
+export type InputComponentProps =
+    | React.DetailedHTMLProps<
+          React.TextareaHTMLAttributes<HTMLTextAreaElement>,
+          HTMLTextAreaElement
+      >
+    | React.DetailedHTMLProps<
+          React.InputHTMLAttributes<HTMLInputElement>,
+          HTMLInputElement
+      >;
