446931af5e7e37f08fad73d24255edd59ce2228f
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 13:17:46 +0330

    add styles of comment input box

--- src/components/CommentInputBox/CommentInputBox.tsx
+++ src/components/CommentInputBox/CommentInputBox.tsx
@@ -1,7 +1,18 @@
 import React, { FunctionComponent } from "react";
 import { CommentInputBoxComponentProps } from "./CommentInputBox.types";
 import s from "./CommentInputBox.module.scss";
+import { Button } from "components/Button";
 
 export const CommentInputBox: FunctionComponent<CommentInputBoxComponentProps> = props => {
-    return <div></div>;
+    return (
+        <div className={s.box}>
+            <p className={s.title}>نظرتون در مورد این پست چیه؟</p>
+            <textarea
+                rows={7}
+                className={s.input}
+                placeholder="مثلا: من این کتاب رو خیلی دوست دارم :)"
+            />
+            <Button type="primary">ارسال نظر</Button>
+        </div>
+    );
 };
