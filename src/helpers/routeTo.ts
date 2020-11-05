ab83669692f6fcaff2875960fcdab4fd31f7a465
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-06 01:31:06 +0330

    update utility types

--- src/helpers/routeTo.ts
+++ src/helpers/routeTo.ts
@@ -5,12 +5,13 @@ import __ROUTES__ from "constants/routes";
 
 type PathType = keyof typeof __ROUTES__;
 type ParamsType = { [paramName: string]: string | number | boolean };
-
-export const routeTo = (
+type RouteToFnType = (
     path: PathType,
     params?: ParamsType,
     queryString?: object
-) => {
+) => string;
+
+export const routeTo: RouteToFnType = (path, params, queryString?) => {
     const url = generatePath(get(__ROUTES__, path), params);
     return queryString ? `${url}?${stringify(queryString)}` : url;
 };
