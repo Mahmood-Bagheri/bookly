0f7327da283184bbebbf909d4c5a947d7be74150
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 18:32:13 +0330

    add dropdown

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,7 @@
+import React, { FunctionComponent } from "react";
+import { DropdownComponentProps } from './Dropdown.types'
+import s from './Dropdown.module.scss'
+
+export const Dropdown: FunctionComponent<DropdownComponentProps> = props => {
+    return <div></div>;
+    };
