2d8b6822b54afbc1f400e89108720f6b2966a377
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-05 17:45:53 +0330

    add books grid components

--- src/components/BooksGrid/BooksGrid.tsx
+++ src/components/BooksGrid/BooksGrid.tsx
@@ -1,7 +1,14 @@
 import React, { FunctionComponent } from "react";
 import { BooksGridComponentProps } from "./BooksGrid.types";
-import s from "./BooksGrid.module.scss";
+import { BookBox } from "components/BookBox";
+import { fakeArrayGenerator } from "helpers/fakeArrayGenerator";
 
 export const BooksGrid: FunctionComponent<BooksGridComponentProps> = props => {
-    return <div></div>;
+    return <div className="row">{fakeArrayGenerator(12).map(renderBooks)}</div>;
 };
+
+const renderBooks = (item: unknown) => (
+    <div className="col-lg-6 col-xl-3 mb-3">
+        <BookBox />
+    </div>
+);
