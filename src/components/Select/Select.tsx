b3b045de07b1e0e81bf92365a9e8acfe6a900831
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-07 21:34:30 +0330

    update select component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,26 +1,20 @@
 import React, { FunctionComponent } from "react";
 /* components */
 import { Select as AntDesignSelect } from "antd";
+/* modules */
+import clsx from "classnames";
 /* types */
 import { SelectComponentProps } from "./Select.types";
 /* styles */
 import s from "./Select.module.scss";
 
-const { Option } = AntDesignSelect;
-
 export const Select: FunctionComponent<SelectComponentProps> = props => {
-    const { options = [] } = props;
+    const { className, ...restProps } = props;
 
     return (
-        <AntDesignSelect className={s.select}>
-            {options.map(option => (
-                <Option
-                    key={option?.value}
-                    value={option?.value as React.ReactText}
-                >
-                    {option?.label}
-                </Option>
-            ))}
-        </AntDesignSelect>
+        <AntDesignSelect
+            className={clsx(s.select, className)}
+            {...restProps}
+        ></AntDesignSelect>
     );
 };
--- src/components/Select/Select.types.d.ts
+++ src/components/Select/Select.types.d.ts
@@ -1,8 +1,8 @@
+import { SelectProps } from "antd/lib/select";
+
 export type OptionType = {
     value: string | number;
     label: string;
 };
 
-export type SelectComponentProps = {
-    options: Partial<OptionType[]>;
-};
+export type SelectComponentProps = SelectProps;
