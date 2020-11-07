6c3518a376af2c30b390c4968e1c3dc53a74a87c
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-07 21:17:10 +0330

    add active classname to menuBarItem

--- src/components/MenuBarItem/MenuBarItem.tsx
+++ src/components/MenuBarItem/MenuBarItem.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from "react";
 /* components */
 /* modules */
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import clsx from "classnames";
 /* helpers */
 /* assets */
@@ -14,7 +14,9 @@ export const MenuBarItem: FunctionComponent<MenuBarItemComponentProps> = props =
     const { className, title, link, ...restProps } = props;
     return (
         <span className={clsx(s.menuItem, className)} {...restProps}>
-            <Link to={link}>{title}</Link>
+            <NavLink activeClassName={s.activeMenuItem} to={link}>
+                {title}
+            </NavLink>
         </span>
     );
 };
