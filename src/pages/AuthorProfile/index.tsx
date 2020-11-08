3add5803a62d535ab3cfca17f9e052a480a268de
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-08 20:51:46 +0330

    update export components of pages

---
+++ src/pages/AuthorProfile/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { loadable } from "helpers/loadable";
+import { LoadingIndicator } from "components/LoadingIndicator";
+
+export default loadable(() => import("./AuthorProfile"), {
+    fallback: <LoadingIndicator />,
+});
