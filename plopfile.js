6ef76efb017cfa826ec454b8fc23c72979f57561
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-08 20:30:39 +0330

    add page plop template

--- plopfile.js
+++ plopfile.js
@@ -1,59 +1,43 @@
 module.exports = plop => {
-    plop.setGenerator("component", {
+    /* plop.setGenerator("component", {
         description: "Create a component",
-        // User input prompts provided as arguments to the template
         prompts: [
             {
-                // Raw text input
                 type: "input",
-                // Variable name for this input
                 name: "name",
-                // Prompt to display on command line
                 message: "What is your component name?",
             },
         ],
         actions: [
             {
-                // Add a new file
                 type: "add",
-                // Path for the new file
                 path:
                     "src/components/{{pascalCase name}}/{{pascalCase name}}.tsx",
-                // Handlebars template used to generate content of new file
                 templateFile:
                     "internals/plop-templates/component/component.tsx.hbs",
             },
             {
-                // Add a new file
                 type: "add",
-                // Path for the new file
                 path:
                     "src/components/{{pascalCase name}}/{{pascalCase name}}.module.scss",
-                // Handlebars template used to generate content of new file
                 templateFile:
                     "internals/plop-templates/component/styles.module.scss.hbs",
             },
             {
-                // Add a new file
                 type: "add",
-                // Path for the new file
                 path:
                     "src/components/{{pascalCase name}}/{{pascalCase name}}.types.d.ts",
-                // Handlebars template used to generate content of new file
                 templateFile:
                     "internals/plop-templates/component/component.types.d.ts.hbs",
             },
             {
-                // Add a new file
                 type: "add",
-                // Path for the new file
                 path: "src/components/{{pascalCase name}}/index.ts",
-                // Handlebars template used to generate content of new file
                 templateFile: "internals/plop-templates/component/index.ts.hbs",
             },
         ],
-    });
-    plop.setGenerator("hook", {
+    }); */
+    /*  plop.setGenerator("hook", {
         description: "Create a hook",
         prompts: [
             {
@@ -70,6 +54,29 @@ module.exports = plop => {
                 path: "src/hooks/use{{pascalCase name}}.ts",
                 // Handlebars template used to generate content of new file
                 templateFile: "internals/plop-templates/hook/hook.ts.hbs",
+            },
+        ],
+    }); */
+
+    plop.setGenerator("page", {
+        description: "Create a page",
+        prompts: [
+            {
+                type: "input",
+                name: "name",
+                message: "What is your page name?",
+            },
+        ],
+        actions: [
+            {
+                type: "add",
+                path: "src/pages/{{pascalCase name}}/{{pascalCase name}}.tsx",
+                templateFile: "internals/plop-templates/page/page.tsx.hbs",
+            },
+            {
+                type: "add",
+                path: "src/pages/{{pascalCase name}}/index.ts",
+                templateFile: "internals/plop-templates/page/index.ts.hbs",
             },
         ],
     });
