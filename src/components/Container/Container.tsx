7791c0c5b31097dc15a6925eb42d51c6ac5a554c
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-09 09:32:17 +0330

    add container component

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from "react";
+/* components */
+/* modules */
+import clsx from "classnames";
+/* helpers */
+/* assets */
+/* types */
+import { ContainerComponentProps } from "./Container.types";
+/* styles */
+import s from "./Container.module.scss";
+
+export const Container: FunctionComponent<ContainerComponentProps> = ({
+    children,
+}) => {
+    return <div className="container">{children}</div>;
+};
