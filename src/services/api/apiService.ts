3a2b5c494aa5887a24ed1dd635ad31bc892cfa4d
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-09 10:18:51 +0330

    add nprogress

--- src/services/api/apiService.ts
+++ src/services/api/apiService.ts
@@ -4,6 +4,7 @@ import { applyAuthInterceptor } from "./interceptors/auth";
 import { applyAccessTokenRevalidator } from "./interceptors/tokenRevalidator";
 import { applyErrorHandler } from "./interceptors/errorHandler";
 import { applyResponseNormalizer } from "./interceptors/responseNormalizer";
+import { applyNprogress } from "./interceptors/nprogress";
 
 const apiService = axios.create({
     baseURL: process.env.REACT_APP_API_URL,
@@ -13,6 +14,7 @@ applyAuthInterceptor(apiService);
 applyAccessTokenRevalidator(apiService);
 applyErrorHandler(apiService);
 applyResponseNormalizer(apiService);
+applyNprogress(apiService);
 
 export * from "axios";
 export default apiService;
