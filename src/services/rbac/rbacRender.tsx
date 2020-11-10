240541c5aae2ddb2b7556dd816ebf3adb363129c
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 21:14:19 +0330

    update rbac render ternary statement

--- src/services/rbac/rbacRender.tsx
+++ src/services/rbac/rbacRender.tsx
@@ -5,9 +5,5 @@ import { usePermissions } from "./usePermissions";
 export const rbacRender = (component: JSX.Element, path: string) => {
     const { role, permissions } = usePermissions();
     const canPerform = get(permissions, `${role}.${path}`);
-
-    if (canPerform) {
-        return component;
-    }
-    return null;
+    return canPerform ? component : null;
 };
