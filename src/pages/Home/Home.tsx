5ebd00f69eed72e7cdce76e4ed3f3a99ec6d0f03
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 10:54:21 +0330

    update home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
 import React, { FC, Fragment } from "react";
 /* components */
+import { DocumentTitle } from "components/DocumentTitle";
+import { BooksGrid } from "components/BooksGrid";
+/* modules */
+import { fakeArrayGenerator } from "helpers/fakeArrayGenerator";
 
 const Home: FC = props => {
-    return <Fragment></Fragment>;
+    const books = fakeArrayGenerator(12);
+    return (
+        <Fragment>
+            <DocumentTitle title="homepage" />
+            <BooksGrid books={books as any} loading={false} />
+        </Fragment>
+    );
 };
 
 export default Home;
