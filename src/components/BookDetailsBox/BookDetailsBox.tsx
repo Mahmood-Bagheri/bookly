859c33b19422a38e27b14155b4b7fac03722f84b
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 10:49:43 +0330

    make book details box dynamic

--- src/components/BookDetailsBox/BookDetailsBox.tsx
+++ src/components/BookDetailsBox/BookDetailsBox.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent } from "react";
 /* components */
 import { BooksCarousel } from "components/BooksCarousel";
+import { Image } from "components/Image";
+/* assets */
+import BookImage from "assets/images/book.jpg";
 /* modules */
 import clsx from "classnames";
 /* types */
@@ -8,14 +11,18 @@ import { BookDetailsBoxComponentProps } from "./BookDetailsBox.types";
 /* styles */
 import s from "./BookDetailsBox.module.scss";
 
-export const BookDetailsBox: FunctionComponent<BookDetailsBoxComponentProps> = props => {
+export const BookDetailsBox: FunctionComponent<BookDetailsBoxComponentProps> = ({
+    title,
+    author,
+    imageSrc = BookImage,
+}) => {
     return (
         <div className={clsx(s.box, `shadow`)}>
             <div className="d-flex justify-content-between align-items-center p-3">
-                <p className={s.title}>کتاب جز از کل </p>
-                <div className={s.author}>استیو تولتز</div>
+                <p className={s.title}>{title}</p>
+                <div className={s.author}>{author}</div>
             </div>
-            <BooksCarousel />
+            <Image className={s.image} src={imageSrc} />
         </div>
     );
 };
--- src/components/BookDetailsBox/BookDetailsBox.types.d.ts
+++ src/components/BookDetailsBox/BookDetailsBox.types.d.ts
@@ -1,1 +1,5 @@
-export type BookDetailsBoxComponentProps = {};
+export type BookDetailsBoxComponentProps = {
+    title: string;
+    author: string;
+    imageSrc?: string;
+};
