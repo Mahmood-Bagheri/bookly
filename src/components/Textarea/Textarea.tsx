3fb987e49a909819bb6f931e27e3df4f6604a53d
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 10:22:22 +0330

    update textarea component

--- src/components/Textarea/Textarea.tsx
+++ src/components/Textarea/Textarea.tsx
@@ -1,9 +1,18 @@
 import React, { FunctionComponent } from "react";
 /* components */
 import { Input } from "antd";
+/* modules */
+import clsx from "classnames";
 /* types */
 import { TextareaComponentProps } from "./Textarea.types";
+/* styles */
+import s from "./Textarea.module.scss";
 
-export const Textarea: FunctionComponent<TextareaComponentProps> = props => {
-    return <Input.TextArea {...props} />;
+export const Textarea: FunctionComponent<TextareaComponentProps> = ({
+    className,
+    ...restProps
+}) => {
+    return (
+        <Input.TextArea className={clsx(s.input, className)} {...restProps} />
+    );
 };
