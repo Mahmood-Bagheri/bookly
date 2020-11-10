4ecde08adea44610ae82e02c9bbb15c4260b2ca2
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 10:53:59 +0330

    update book page

--- src/pages/Book/Book.tsx
+++ src/pages/Book/Book.tsx
@@ -1,8 +1,45 @@
 import React, { FC, Fragment } from "react";
 /* components */
+import { BookDetailsBox } from "components/BookDetailsBox";
+import { BookDescriptionText } from "components/BookDescriptionText";
+import { CommentsGrid } from "components/CommentsGrid";
+import { CommentInputBox } from "components/CommentInputBox";
+import { Container } from "components/Container";
+/* modules */
+import { randomDate } from "helpers/randomDate";
 
 const Book: FC = props => {
-    return <Fragment></Fragment>;
+    const comments = [
+        {
+            username: "سمیه",
+            date: randomDate(new Date("2020/1/1")),
+            body: "خیلی خوب بود :)",
+        },
+    ];
+    return (
+        <Container>
+            <div className="row">
+                <div className="col-xl-4">
+                    <BookDetailsBox
+                        title="کتاب جز از کل"
+                        author="استیو تولتز"
+                    />
+                    <CommentInputBox
+                        onSubmit={comment => console.log(comment)}
+                    />
+                </div>
+                <div className="col-xl-8 mt-3 mt-xl-0">
+                    <BookDescriptionText
+                        description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد
+                                      "
+                    />
+                    <CommentsGrid comments={comments} />
+                </div>
+            </div>
+        </Container>
+    );
 };
 
 export default Book;
