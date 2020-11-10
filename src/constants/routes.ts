2513a6ad957efe6f8dae645c6f198855c29dd4f6
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-10 19:23:39 +0330

    add in-mind route organizing

--- src/constants/routes.ts
+++ src/constants/routes.ts
@@ -8,3 +8,18 @@ const __ROUTES__ = {
 };
 
 export default __ROUTES__;
+
+// const __ROUTES__ = {
+//     books: "/books",
+//     book: "/books/:bookId",
+//     publishers: "/publishers",
+//     publisher: "/publishers/:publisherId",
+
+//     userProfile: "/user/profile",
+//     userProfileComments: "/user/profile/comments",
+//     userProfileLikedBooks: "/user/profile/liked-books",
+
+//     authorProfile: "/author/profile",
+
+//     error: "/error",
+// };
