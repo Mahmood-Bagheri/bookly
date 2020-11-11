1b641f667a92bc0239414052ac34646afbc3ff44
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-11 20:20:07 +0330

    add react-lazy-load-image-component to imageComponent

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,11 +1,30 @@
 import React, { FunctionComponent } from "react";
+/* components */
+import {
+    LazyLoadImage,
+    LazyLoadImageProps,
+} from "react-lazy-load-image-component";
+/* modules */
+import clsx from "classnames";
 /* types */
 import { ImageComponentProps } from "./Image.types";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
 /* styles */
 import s from "./Image.module.scss";
 
-export const Image: FunctionComponent<ImageComponentProps> = props => {
-    const defaultAltText = "This image is related to the website :/";
-    const { alt = defaultAltText, ...restProps } = props;
-    return <img alt={alt} {...restProps} />;
+const defaultAltText = "This image is related to the website :/";
+export const Image: FunctionComponent<LazyLoadImageProps> = ({
+    className,
+    alt = defaultAltText,
+    ...restProps
+}) => {
+    return (
+        <LazyLoadImage
+            effect="blur"
+            alt={alt}
+            className={className}
+            {...restProps}
+        />
+    );
 };
