bb209588cede3af2ff994827da1dcd2ecd11203d
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-15 13:13:44 +0330

    move link and cols of booksgrid to internal bookbox component

--- src/components/BookBox/BookBox.tsx
+++ src/components/BookBox/BookBox.tsx
@@ -2,7 +2,10 @@ import React from "react";
 /* components */
 import { LikeButton } from "components/LikeButton";
 import { Image } from "components/Image";
-
+import { Col } from "components/Col";
+/* modules */
+import { Link } from "react-router-dom";
+import { routeTo } from "helpers/routeTo";
 /* assets */
 import BookImage from "assets/images/book.jpg";
 /* types */
@@ -25,25 +28,29 @@ export const BookBox: BookBoxComponentType = props => {
     } = props;
 
     return (
-        <div className={`${s.box} shadow`} {...restProps}>
-            <Image className={s.image} src={imageSrc} />
-            <div className={s.content}>
-                <div>
-                    <div className={s.title}>{title}</div>
+        <Col lg={6} xl={3} className="mb-3">
+            <Link to={routeTo("error", { id: "Salam" })}>
+                <div className={`${s.box} shadow`} {...restProps}>
+                    <Image className={s.image} src={imageSrc} />
+                    <div className={s.content}>
+                        <div>
+                            <div className={s.title}>{title}</div>
+                        </div>
+                        <div>
+                            <div className={s.author}>{author}</div>
+                            <div className={s.authorTitle}>نویسنده</div>
+                        </div>
+                    </div>
+                    <div className={s.actions}>
+                        <AclService permission="book.like">
+                            <LikeButton
+                                onLikeStateChange={onLikeStateChange}
+                                initialLikeState={initialLikeState}
+                            />
+                        </AclService>
+                    </div>
                 </div>
-                <div>
-                    <div className={s.author}>{author}</div>
-                    <div className={s.authorTitle}>نویسنده</div>
-                </div>
-            </div>
-            <div className={s.actions}>
-                <AclService permission="book.like">
-                    <LikeButton
-                        onLikeStateChange={onLikeStateChange}
-                        initialLikeState={initialLikeState}
-                    />
-                </AclService>
-            </div>
-        </div>
+            </Link>
+        </Col>
     );
 };
--- src/components/BooksGrid/BooksGrid.tsx
+++ src/components/BooksGrid/BooksGrid.tsx
@@ -3,15 +3,10 @@ import React, { FunctionComponent } from "react";
 import { BookBox } from "components/BookBox";
 import { Container } from "components/Container";
 import { LoadingIndicator } from "components/LoadingIndicator";
-/* assets */
-import BookImage from "assets/images/book.jpg";
 /* types */
 import { BooksGridComponentProps } from "./BooksGrid.types";
 /* styles */
 import s from "./BooksGrid.module.scss";
-import { Link } from "react-router-dom";
-import { routeTo } from "helpers/routeTo";
-import { Col } from "components/Col";
 
 export const BooksGrid: FunctionComponent<BooksGridComponentProps> = ({
     title = "کتاب ها",
@@ -31,17 +26,13 @@ export const BooksGrid: FunctionComponent<BooksGridComponentProps> = ({
 };
 
 const renderBooks = (item: unknown, index: number) => (
-    <Col lg={6} xl={3} className="mb-3">
-        <Link to={routeTo("error", { id: "Salam" })}>
-            <BookBox
-                title="جز از کل"
-                author="استیو تولتز"
-                imageSrc={`https://source.unsplash.com/500x500/?book&sig=${index}`}
-                canLike={false}
-                onBookmarkStateChange={() => {}}
-                onLikeStateChange={() => {}}
-                initialLikeState={false}
-            />
-        </Link>
-    </Col>
+    <BookBox
+        title="جز از کل"
+        author="استیو تولتز"
+        imageSrc={`https://source.unsplash.com/500x500/?book&sig=${index}`}
+        canLike={false}
+        onBookmarkStateChange={() => {}}
+        onLikeStateChange={() => {}}
+        initialLikeState={false}
+    />
 );
