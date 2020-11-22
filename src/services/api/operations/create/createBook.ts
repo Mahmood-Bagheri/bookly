c128bfb059392348ce0e29c98ba3f1dc9b49e714
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-22 17:06:05 +0330

    add some api mutatiosn

--- src/services/api/operations/create/createBook.ts
+++ src/services/api/operations/create/createBook.ts
@@ -1,6 +1,11 @@
 import __API_URLS__ from "constants/apiUrls";
+import { useMutation } from "react-query";
 import apiService from "services/api/apiService";
 
 type CreateBookTypes = {};
-export const createBook = (data: CreateBookTypes) =>
-    apiService.post(__API_URLS__.book, data);
+
+export const createBook = (data: CreateBookTypes) => {
+    return apiService.post(__API_URLS__.book, data);
+};
+
+export const useCreateBook = () => useMutation(createBook);
--- src/services/api/operations/delete/deleteBook.ts
+++ src/services/api/operations/delete/deleteBook.ts
@@ -1,6 +1,11 @@
+import { useMutation } from "react-query";
 import __API_URLS__ from "constants/apiUrls";
 import apiService from "services/api/apiService";
 
 type DeleteBookType = {};
-export const deleteBook = (data: DeleteBookType) =>
-    apiService.delete(__API_URLS__.book, data);
+
+export const deleteBook = (data: DeleteBookType) => {
+    return apiService.delete(__API_URLS__.book, data);
+};
+
+export const useDeleteBook = () => useMutation(deleteBook);
