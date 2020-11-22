898e5f1be3d440b573f2d18e8c12d95da68b5820
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-22 16:46:32 +0330

    update readBook api operations

--- src/services/api/operations/read/readBook.ts
+++ src/services/api/operations/read/readBook.ts
@@ -1,6 +1,14 @@
+import { useQuery } from "react-query";
 import __API_URLS__ from "constants/apiUrls";
 import apiService from "services/api/apiService";
 
-type ReadBookType = {};
-export const readBook = (params: ReadBookType) =>
-    apiService.get(__API_URLS__.book, { params });
+export const readBook = async (_: unknown, bookId: string) => {
+    const { data } = await apiService.get(__API_URLS__.book, {
+        params: { bookId },
+    });
+    return data;
+};
+
+export const useSingleBook = (bookId: string) => {
+    return useQuery([__API_URLS__.book, bookId], readBook);
+};
