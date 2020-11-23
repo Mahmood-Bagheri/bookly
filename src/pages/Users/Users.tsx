72773cc4ee90ea911f64284439094b8fa8281c57
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-23 19:42:15 +0330

    add deleteUser to props of users table

--- src/pages/Users/Users.tsx
+++ src/pages/Users/Users.tsx
@@ -2,12 +2,15 @@ import React, { FC, Fragment } from "react";
 /* components */
 import { DocumentTitle } from "components/DocumentTitle";
 import { UsersTable } from "components/UsersTable";
+import { useDeleteUser } from "services/api/operations";
 
 const Users: FC = props => {
+    const [deleteUser] = useDeleteUser();
+
     return (
         <Fragment>
             <DocumentTitle title="کاربران" />
-            <UsersTable />
+            <UsersTable onDeleteUser={deleteUser} />
         </Fragment>
     );
 };
