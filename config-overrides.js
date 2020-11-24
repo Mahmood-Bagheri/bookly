4a1a5960acde996e38a5b1ebe812267eccfd2d4b
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-24 19:11:07 +0330

    add webpackbar

--- config-overrides.js
+++ config-overrides.js
@@ -1,10 +1,15 @@
-const { override, addLessLoader } = require("customize-cra");
-
+const { override, addLessLoader, addWebpackPlugin } = require("customize-cra");
+const WebpackBar = require("webpackbar");
 module.exports = override(
     addLessLoader({
         lessOptions: {
             javascriptEnabled: true,
             modifyVars: { "@primary-color": "#000" },
         },
-    })
+    }),
+    addWebpackPlugin(
+        new WebpackBar({
+            color: "#eb34c6",
+        })
+    )
 );
