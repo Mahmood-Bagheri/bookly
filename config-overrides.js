cf2bc8a3049496a499c8714ae4ed65cd8eda0885
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-24 19:18:55 +0330

    update config

--- config-overrides.js
+++ config-overrides.js
@@ -1,15 +1,17 @@
 const { override, addLessLoader, addWebpackPlugin } = require("customize-cra");
 const WebpackBar = require("webpackbar");
-module.exports = override(
-    addLessLoader({
-        lessOptions: {
-            javascriptEnabled: true,
-            modifyVars: { "@primary-color": "#000" },
-        },
-    }),
-    addWebpackPlugin(
-        new WebpackBar({
-            color: "#eb34c6",
-        })
-    )
+
+const lessLoaders = addLessLoader({
+    lessOptions: {
+        javascriptEnabled: true,
+        modifyVars: { "@primary-color": "#000" },
+    },
+});
+
+const WebpackPlugins = addWebpackPlugin(
+    new WebpackBar({
+        color: "#eb34c6",
+    })
 );
+
+module.exports = override(lessLoaders, WebpackPlugins);
