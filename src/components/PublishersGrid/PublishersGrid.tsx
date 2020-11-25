f5d8e87a5ff0ff026368cdb4f1a1eb2fe8700e11
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-25 12:44:03 +0330

    apply GenericGrid

--- src/components/PublishersGrid/PublishersGrid.tsx
+++ src/components/PublishersGrid/PublishersGrid.tsx
@@ -1,29 +1,34 @@
 import React, { FunctionComponent } from "react";
 /* components */
+import { PublisherBox } from "components/PublisherBox";
+import { GenericGrid } from "components/GenericGrid";
 /* modules */
+import { fakeArrayGenerator } from "helpers/fakeArrayGenerator";
 import clsx from "classnames";
 /* types */
 import { PublishersGridComponentProps } from "./PublishersGrid.types";
 /* styles */
 import s from "./PublishersGrid.module.scss";
-import { fakeArrayGenerator } from "helpers/fakeArrayGenerator";
-import { PublisherBox } from "components/PublisherBox";
-import { Container } from "components/Container";
-import { Row } from "components/Row";
 
 export const PublishersGrid: FunctionComponent<PublishersGridComponentProps> = props => {
     const { className, ...restProps } = props;
-    return <Row>{fakeArrayGenerator(10).map(renderPublisher)}</Row>;
+    return (
+        <GenericGrid
+            withRow
+            items={fakeArrayGenerator(10)}
+            renderItem={renderPublisher}
+        />
+    );
 };
 
-const renderPublisher = (item: unknown, index: number) => (
+const renderPublisher = (item: any) => (
     <PublisherBox
         id="123"
         title="ققنوس"
         description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
                 استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
                 در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد"
-        imageSrc={`https://source.unsplash.com/500x500/?book&sig=${index}`}
+        imageSrc={`https://source.unsplash.com/500x500/?book&sig=1`}
         initialFollowingState={false}
         onFollow={publisherId => console.log(publisherId)}
     />
