03f3607d068dfa6349e1c3a5513a4013af5fa1ab
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-25 12:39:19 +0330

    add support of row to genericGrid component

--- src/components/GenericGrid/GenericGrid.tsx
+++ src/components/GenericGrid/GenericGrid.tsx
@@ -1,11 +1,14 @@
 import * as React from "react";
+import { Row } from "components/Row";
 
 interface Props<T> {
     items: T[];
     renderItem: (item: T) => React.ReactNode;
+    withRow?: boolean;
 }
 
 export function GenericGrid<T>(props: Props<T>) {
-    const { items, renderItem } = props;
-    return <React.Fragment>{items.map(renderItem)}</React.Fragment>;
+    const { items, renderItem, withRow = false } = props;
+    const RowPlaceholder = withRow ? Row : React.Fragment;
+    return <RowPlaceholder>{items.map(renderItem)}</RowPlaceholder>;
 }
