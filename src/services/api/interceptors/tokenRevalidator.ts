1f4be4be86b97b6e35aba6fef24b9dc5476857c3
Mahmud Bagheri <dr.cwna@gmail.com>
2020-11-30 12:30:24 +0330

    add api_status_codes

--- src/services/api/interceptors/tokenRevalidator.ts
+++ src/services/api/interceptors/tokenRevalidator.ts
@@ -1,12 +1,15 @@
 import { AxiosInstance, AxiosResponse, AxiosError } from "axios";
+import { API_STATUS_CODES } from "constants/statusCodes";
 
 export const applyAccessTokenRevalidator = (instance: AxiosInstance) => {
     return instance.interceptors.response.use(
         (res: AxiosResponse) => res,
         (error: AxiosError) => {
             if (error.response) {
-                const UNAUTHORIZED_STATUS_CODE = 401;
-                if (error?.response?.status === UNAUTHORIZED_STATUS_CODE) {
+                if (
+                    error?.response?.status ===
+                    API_STATUS_CODES.UNAUTHORIZED_STATUS_CODE
+                ) {
                     // * make an api call to revalidate token
                 }
             }
