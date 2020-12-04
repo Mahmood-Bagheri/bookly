7feb2c45dfd49dc772cf6d3a2a3dc00a533b2248
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-04 17:13:57 +0330

    update exports

--- src/components/CommentBox/index.ts
+++ src/components/CommentBox/index.ts
@@ -1,8 +1,5 @@
 import { CommentBox as Component } from "./CommentBox";
 import { CommentBoxShimmer as Shimmer } from "./CommentBoxShimmer";
 import { CommentBoxShimmerGrid as ShimmerGrid } from "./CommentBoxShimmerGrid";
-export const CommentBox = Object.assign(
-    {},
-    { Component, Shimmer, ShimmerGrid }
-);
+export const CommentBox = { Component, Shimmer, ShimmerGrid };
 export * from "./CommentBox.types.d";
--- src/components/PublisherBox/index.ts
+++ src/components/PublisherBox/index.ts
@@ -1,13 +1,5 @@
 import { PublisherBox as Component } from "./PublisherBox";
 import { PublisherBoxShimmer as Shimmer } from "./PublisherBoxShimmer";
 import { PublisherBoxShimmerGrid as ShimmerGrid } from "./PublisherBoxShimmerGrid";
-
-export const PublisherBox = Object.assign(
-    {},
-    {
-        Component,
-        Shimmer,
-        ShimmerGrid,
-    }
-);
+export const PublisherBox = { Component, Shimmer, ShimmerGrid };
 export * from "./PublisherBox.types.d";
