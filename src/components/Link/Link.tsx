89923fb52dc9a3a5911767efcf2b2b345683ef62
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-06 11:03:44 +0330

    make link component work as well!

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -5,6 +5,48 @@ import { AclService } from "services/rbac";
 type LinkProps = {
     permission: string;
 };
+export type AnchorProps = JSX.IntrinsicElements["a"];
+export type RouterLinkProps = Omit<NavLinkProps, "href"> & LinkProps;
+
+function isPropsForRouterLink(
+    props: AnchorProps | RouterLinkProps
+): props is RouterLinkProps {
+    return "permission" in props;
+}
+
+// todo -> why the intellisense not working ?! maybe a type mismatch ...
+export const Link = <T extends {}>(
+    props: T extends RouterLinkProps ? RouterLinkProps & LinkProps : AnchorProps
+) => {
+    if (isPropsForRouterLink(props)) {
+        return (
+            <AclService permission={props.permission}>
+                <NavLink {...(props as RouterLinkProps)} />
+            </AclService>
+        );
+    } else {
+        return <a {...(props as AnchorProps)} />;
+    }
+};
+
+// const Usage = () => {
+//     return (
+//         <div>
+//             <Link<RouterLinkProps> to="/">My link</Link>; // ok
+//             <Link<AnchorProps> href="/">My link</Link>; // ok
+//             <Link<RouterLinkProps> to="/">My link</Link>; // error
+//         </div>
+//     );
+// };
+
+/* // deprecated
+import * as React from "react";
+import { NavLink, NavLinkProps } from "react-router-dom";
+import { AclService } from "services/rbac";
+
+type LinkProps = {
+    permission: string;
+};
 type RouterLinkProps = Omit<NavLinkProps, "href"> & LinkProps;
 
 export const Link = (props: LinkProps & RouterLinkProps) => {
@@ -14,34 +56,4 @@ export const Link = (props: LinkProps & RouterLinkProps) => {
         </AclService>
     );
 };
-
-/* 
-
-import * as React from "react";
-import { NavLink, NavLinkProps } from "react-router-dom";
-
-interface LinkProps {}
-type AnchorProps = React.AnchorHTMLAttributes<HTMLAnchorElement>;
-type RouterLinkProps = Omit<NavLinkProps, "href">;
-
-export const Link = <T extends {}>(
-    props: LinkProps & T extends RouterLinkProps ? RouterLinkProps : AnchorProps
-) => {
-    if ((props as RouterLinkProps).to) {
-        return <NavLink {...(props as RouterLinkProps)} />;
-    } else {
-        return <a {...(props as AnchorProps)} />;
-    }
-};
-
-const Usage = () => {
-    return (
-        <div>
-            <Link<RouterLinkProps> to="/">My link</Link>; // ok
-            <Link<AnchorProps> href="/">My link</Link>; // ok
-            <Link<RouterLinkProps> to="/">My link</Link>; // error
-        </div>
-    );
-};
-
-*/
+ */
