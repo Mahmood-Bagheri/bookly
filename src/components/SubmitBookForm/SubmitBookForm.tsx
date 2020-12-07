591a54f0d143e6353c751576dafc52c5571e77d4
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-07 15:18:35 +0330

    update

--- src/components/SubmitBookForm/SubmitBookForm.tsx
+++ src/components/SubmitBookForm/SubmitBookForm.tsx
@@ -65,7 +65,7 @@ export const SubmitBookForm: FC<SubmitBookFormProps> = ({
                             label="عکس کتاب"
                             valuePropName="file"
                         >
-                            <UploadDropbox {...dropBoxConfig} />
+                            <UploadDropbox />
                         </Form.Item>
 
                         <Form.Item name="tag" label="دسته بندی">
