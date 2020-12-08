afc27ce621bb28c84681b4972953febca83b4b50
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-08 17:00:28 +0330

    show emptyGrid on nothing found in generic grid

--- src/components/GenericGrid/GenericGrid.tsx
+++ src/components/GenericGrid/GenericGrid.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
 import { Row } from "components/Row";
+import { isEmptyArray } from "helpers/isEmptyArray";
+import { EmptyGrid } from "components/EmptyGrid";
 
 type Props<T> = {
     items: T[];
@@ -9,14 +11,14 @@ type Props<T> = {
 };
 
 export function GenericGrid<T>(props: Props<T>) {
-    const {
-        items,
-        renderItem,
-        filter = () => true,
-        withRow: shouldAddRowTagBeforeGrid = false,
-    } = props;
-    const RowPlaceholder = shouldAddRowTagBeforeGrid ? Row : React.Fragment;
-    return (
-        <RowPlaceholder>{items.filter(filter).map(renderItem)}</RowPlaceholder>
-    );
+    const { items, renderItem, filter = () => true, withRow = false } = props;
+
+    const RowPlaceholder = withRow ? Row : React.Fragment;
+    const filtered = items.filter(filter);
+
+    if (isEmptyArray(filtered)) {
+        return <EmptyGrid />;
+    }
+
+    return <RowPlaceholder>{filtered.map(renderItem)}</RowPlaceholder>;
 }
