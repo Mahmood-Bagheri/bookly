5092921b46d493543f77e7b6c74fbfd160052f87
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-11 11:25:55 +0330

    remove likebox

--- src/components/LikeBox/LikeBox.tsx
+++
@@ -1,7 +0,0 @@
-import React, { FunctionComponent } from "react";
-import { LikeBoxComponentProps } from "./LikeBox.types";
-import s from "./LikeBox.module.scss";
-
-export const LikeBox: FunctionComponent<LikeBoxComponentProps> = props => {
-    return <div></div>;
-};
