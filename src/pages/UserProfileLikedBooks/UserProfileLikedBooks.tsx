edb9c4d7def219eff71981f8c6ab2d55dc1de020
Mahmud Bagheri <dr.cwna@gmail.com>
2020-12-11 11:22:18 +0330

    update imports

--- src/pages/UserProfileLikedBooks/UserProfileLikedBooks.tsx
+++ src/pages/UserProfileLikedBooks/UserProfileLikedBooks.tsx
@@ -1,6 +1,6 @@
 import React, { FC, Fragment } from "react";
 /* components */
-import { BooksGrid } from "components/Book/BooksGrid";
+import { BooksGrid } from "components/Book";
 import { mock } from "helpers/mock";
 
 const UserProfileLikedBooks: FC = props => {
